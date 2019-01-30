f89e0db5554a95190499c56442d24e0e6875ba23
IQbrod <iqbrod@outlook.fr>
2019-01-30 11:20:33 +0100

    Affichage des données

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import torch as tor
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#Definition des tailles
+train_size = 30
+test_size = 5
+
 #Creation du tensor
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -13,13 +21,30 @@ transform = transforms.Compose(
 
 #Import du jeu de donnée (pour Training)
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-#Entraineur: taille du jeu 5 par 5
-trainLoader = tor.utils.data.DataLoader(trainset, batch_size=5, shuffle=True, num_workers=2)
+#Entraineur
+trainLoader = tor.utils.data.DataLoader(trainset, batch_size= train_size, shuffle=True, num_workers=2)
 
 #Import d'un jeu de test (sans Training)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-#Testeur: taille du jeu 3 par 3
-testloader = tor.utils.data.DataLoader(testset, batch_size=3, shuffle=False, num_workers=2)
+#Testeur
+testloader = tor.utils.data.DataLoader(testset, batch_size= test_size, shuffle=False, num_workers=2)
 
 #Definition des classes
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+#Fonction d'affichage d'image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(trainLoader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(train_size)))
